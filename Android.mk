5153ad15fc879d3c50832be0fdac1475df8fa47c
Chris <chris@cypheros.co>
2018-12-04 06:44:40 -0500

    aosip/hw: Refractor handling of framework
    - Added a new library "hardware.aosip.core" which
      will handle /core and /services paths
    - hardware.aosip will now only import /core path since it'll be used as
      a static import for features, we can't declare system services framework
      there
    - hardware.aosip.controllers will now depend on hardware.aosip and
      services
    - Added a CleanSpec
    
    hardware.aosip.core and hardware.aosip.controllers need to be compiled
    as java jars with permissions
    
    Change-Id: Iac1b418c3c219fcdc2bbb5e38e16bf264412ba2e
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- Android.mk
+++ Android.mk
@@ -2,10 +2,18 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+LOCAL_MODULE := hardware.aosip.core.xml
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT)/etc/permissions
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+
 LOCAL_MODULE := hardware.aosip.controllers.xml
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT)/etc/permissions
-
 include $(BUILD_PREBUILT)
