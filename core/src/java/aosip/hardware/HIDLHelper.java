CypherOS/hardware_aoscp_extensions

devicehw: Merge some display mode improvements
From LineageOS https://github.com/LineageOS/android_lineage-sdk/commit/b49ed5560006528f0813838108d9b30fe11b331c#diff-f0704c641e1f0457d7f1f4660f1e4014

Change-Id: I5e1d2d0d75bb0ff857a77cfe774c6d83f88ec19f

core/src/java/aoscp/hardware/HIDLHelper.java
@@ -1,0 +1,40 @@
+/*
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package aoscp.hardware;
+
+import aoscp.hardware.DisplayMode;
+
+import java.util.ArrayList;
+
+class HIDLHelper {
+
+    public static DisplayMode[] fromHIDLModes(
+            ArrayList<vendor.aoscp.displayengine.V1_0.DisplayMode> modes) {
+        int size = modes.size();
+        DisplayMode[] r = new DisplayMode[size];
+        for (int i = 0; i < size; i++) {
+            vendor.aoscp.displayengine.V1_0.DisplayMode m = modes.get(i);
+            r[i] = new DisplayMode(m.id, m.name);
+        }
+        return r;
+    }
+
+    public static DisplayMode fromHIDLMode(
+            vendor.aoscp.displayengine.V1_0.DisplayMode mode) {
+        return new DisplayMode(mode.id, mode.name);
+    }
+}

xlxfoxxlx/platform_hardware_aosip

Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

core/src/java/aosip/hardware/HIDLHelper.java
@@ -1,0 +1,40 @@
+/*
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package aosip.hardware;
+
+import aosip.hardware.DisplayMode;
+
+import java.util.ArrayList;
+
+class HIDLHelper {
+
+    public static DisplayMode[] fromHIDLModes(
+            ArrayList<vendor.aosip.displayengine.V1_0.DisplayMode> modes) {
+        int size = modes.size();
+        DisplayMode[] r = new DisplayMode[size];
+        for (int i = 0; i < size; i++) {
+            vendor.aosip.displayengine.V1_0.DisplayMode m = modes.get(i);
+            r[i] = new DisplayMode(m.id, m.name);
+        }
+        return r;
+    }
+
+    public static DisplayMode fromHIDLMode(
+            vendor.aosip.displayengine.V1_0.DisplayMode mode) {
+        return new DisplayMode(mode.id, mode.name);
+    }
+}