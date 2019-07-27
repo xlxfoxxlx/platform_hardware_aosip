CypherOS/hardware_aoscp_extensions

DeviceHW: Add a dedicated API for DisplayEngine modes
Ported from https://github.com/LineageOS/android_lineage-sdk

Change-Id: I5ea815f7b0f850a9b1bad3d6186a8ff9e198646f

core/java/aoscp/hardware/DisplayMode.java
@@ -1,0 +1,105 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package aoscp.hardware;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import aoscp.os.Concierge;
+import aoscp.os.Concierge.ParcelInfo;
+
+/**
+ * Display Modes API
+ *
+ * A device may implement a list of preset display modes for different
+ * viewing intents, such as movies, photos, or extra vibrance. These
+ * modes may have multiple components such as gamma correction, white
+ * point adjustment, etc, but are activated by a single control point.
+ *
+ * This API provides support for enumerating and selecting the
+ * modes supported by the hardware.
+ *
+ * A DisplayMode is referenced by it's identifier and carries an
+ * associated name (up to the user to translate this value).
+ */
+public class DisplayMode implements Parcelable {
+    public final int id;
+    public final String name;
+
+    public DisplayMode(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    private DisplayMode(Parcel parcel) {
+        // Read parcelable version via the Concierge
+        ParcelInfo parcelInfo = Concierge.receiveParcel(parcel);
+        int parcelableVersion = parcelInfo.getParcelVersion();
+
+        // temp vars
+        int tmpId = -1;
+        String tmpName = null;
+
+        tmpId = parcel.readInt();
+        if (parcel.readInt() != 0) {
+            tmpName = parcel.readString();
+        }
+
+        // set temps
+        this.id = tmpId;
+        this.name = tmpName;
+
+        // Complete parcel info for the concierge
+        parcelInfo.complete();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }   
+
+    @Override
+    public void writeToParcel(Parcel out, int flags) {
+        // Tell the concierge to prepare the parcel
+        ParcelInfo parcelInfo = Concierge.prepareParcel(out);
+
+        out.writeInt(id);
+        if (name != null) {
+            out.writeInt(1);
+            out.writeString(name);
+        } else {
+            out.writeInt(0);
+        }
+
+        // Complete the parcel info for the concierge
+        parcelInfo.complete();
+    }
+    
+    /** @hide */
+    public static final Parcelable.Creator<DisplayMode> CREATOR =
+            new Parcelable.Creator<DisplayMode>() {
+        public DisplayMode createFromParcel(Parcel in) {
+            return new DisplayMode(in);
+        }
+
+        @Override
+        public DisplayMode[] newArray(int size) {
+            return new DisplayMode[size];
+        }
+    };
+
+}

core/java/aoscp/os/Concierge.java
@@ -1,0 +1,145 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package aoscp.os;
+
+import android.os.Parcel;
+
+/**
+ * Simply, Concierge handles your parcels and makes sure they get marshalled and unmarshalled
+ * correctly when cross IPC boundaries even when there is a version mismatch between the client
+ * sdk level and the framework implementation.
+ *
+ * <p>On incoming parcel (to be unmarshalled):
+ *
+ * <pre class="prettyprint">
+ *     ParcelInfo incomingParcelInfo = Concierge.receiveParcel(incomingParcel);
+ *     int parcelableVersion = incomingParcelInfo.getParcelVersion();
+ *
+ *     // Do unmarshalling steps here iterating over every plausible version
+ *
+ *     // Complete the process
+ *     incomingParcelInfo.complete();
+ * </pre>
+ *
+ * <p>On outgoing parcel (to be marshalled):
+ *
+ * <pre class="prettyprint">
+ *     ParcelInfo outgoingParcelInfo = Concierge.prepareParcel(incomingParcel);
+ *
+ *     // Do marshalling steps here iterating over every plausible version
+ *
+ *     // Complete the process
+ *     outgoingParcelInfo.complete();
+ * </pre>
+ */
+public final class Concierge {
+
+    /** Not instantiable */
+    private Concierge() {
+        // Don't instantiate
+    }
+
+    /**
+     * @hide
+     */
+    public static final int PARCELABLE_VERSION = "poundcake";
+
+    /**
+     * Tell the concierge to receive our parcel, so we can get information from it.
+     *
+     * MUST CALL {@link ParcelInfo#complete()} AFTER UNMARSHALLING.
+     *
+     * @param parcel Incoming parcel to be unmarshalled
+     * @return {@link ParcelInfo} containing parcel information, specifically the version.
+     */
+    public static ParcelInfo receiveParcel(Parcel parcel) {
+        return new ParcelInfo(parcel);
+    }
+
+    /**
+     * Prepare a parcel for the Concierge.
+     *
+     * MUST CALL {@link ParcelInfo#complete()} AFTER MARSHALLING.
+     *
+     * @param parcel Outgoing parcel to be marshalled
+     * @return {@link ParcelInfo} containing parcel information, specifically the version.
+     */
+    public static ParcelInfo prepareParcel(Parcel parcel) {
+        return new ParcelInfo(parcel, PARCELABLE_VERSION);
+    }
+
+    /**
+     * Parcel header info specific to the Parcel object that is passed in via
+     * {@link #prepareParcel(Parcel)} or {@link #receiveParcel(Parcel)}. The exposed method
+     * of {@link #getParcelVersion()} gets the api level of the parcel object.
+     */
+    public final static class ParcelInfo {
+        private Parcel mParcel;
+        private int mParcelableVersion;
+        private int mParcelableSize;
+        private int mStartPosition;
+        private int mSizePosition;
+        private boolean mCreation = false;
+
+        ParcelInfo(Parcel parcel) {
+            mCreation = false;
+            mParcel = parcel;
+            mParcelableVersion = parcel.readInt();
+            mParcelableSize = parcel.readInt();
+            mStartPosition = parcel.dataPosition();
+        }
+
+        ParcelInfo(Parcel parcel, int parcelableVersion) {
+            mCreation = true;
+            mParcel = parcel;
+            mParcelableVersion = parcelableVersion;
+
+            // Write parcelable version, make sure to define explicit changes
+            // within {@link #PARCELABLE_VERSION);
+            mParcel.writeInt(mParcelableVersion);
+
+            // Inject a placeholder that will store the parcel size from this point on
+            // (not including the size itself).
+            mSizePosition = parcel.dataPosition();
+            mParcel.writeInt(0);
+            mStartPosition = parcel.dataPosition();
+        }
+
+        /**
+         * Get the parcel version from the {@link Parcel} received by the Concierge.
+         * @return {@link #PARCELABLE_VERSION} of the {@link Parcel}
+         */
+        public int getParcelVersion() {
+            return mParcelableVersion;
+        }
+
+        /**
+         * Complete the {@link ParcelInfo} for the Concierge.
+         */
+        public void complete() {
+            if (mCreation) {
+                // Go back and write size
+                mParcelableSize = mParcel.dataPosition() - mStartPosition;
+                mParcel.setDataPosition(mSizePosition);
+                mParcel.writeInt(mParcelableSize);
+                mParcel.setDataPosition(mStartPosition + mParcelableSize);
+            } else {
+                mParcel.setDataPosition(mStartPosition + mParcelableSize);
+            }
+        }
+    }
+}

xlxfoxxlx/platform_hardware_aosip

Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

core/java/aosip/hardware/DisplayMode.java
@@ -1,0 +1,105 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package aosip.hardware;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import aosip.os.Concierge;
+import aosip.os.Concierge.ParcelInfo;
+
+/**
+ * Display Modes API
+ *
+ * A device may implement a list of preset display modes for different
+ * viewing intents, such as movies, photos, or extra vibrance. These
+ * modes may have multiple components such as gamma correction, white
+ * point adjustment, etc, but are activated by a single control point.
+ *
+ * This API provides support for enumerating and selecting the
+ * modes supported by the hardware.
+ *
+ * A DisplayMode is referenced by it's identifier and carries an
+ * associated name (up to the user to translate this value).
+ */
+public class DisplayMode implements Parcelable {
+    public final int id;
+    public final String name;
+
+    public DisplayMode(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    private DisplayMode(Parcel parcel) {
+        // Read parcelable version via the Concierge
+        ParcelInfo parcelInfo = Concierge.receiveParcel(parcel);
+        int parcelableVersion = parcelInfo.getParcelVersion();
+
+        // temp vars
+        int tmpId = -1;
+        String tmpName = null;
+
+        tmpId = parcel.readInt();
+        if (parcel.readInt() != 0) {
+            tmpName = parcel.readString();
+        }
+
+        // set temps
+        this.id = tmpId;
+        this.name = tmpName;
+
+        // Complete parcel info for the concierge
+        parcelInfo.complete();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }   
+
+    @Override
+    public void writeToParcel(Parcel out, int flags) {
+        // Tell the concierge to prepare the parcel
+        ParcelInfo parcelInfo = Concierge.prepareParcel(out);
+
+        out.writeInt(id);
+        if (name != null) {
+            out.writeInt(1);
+            out.writeString(name);
+        } else {
+            out.writeInt(0);
+        }
+
+        // Complete the parcel info for the concierge
+        parcelInfo.complete();
+    }
+    
+    /** @hide */
+    public static final Parcelable.Creator<DisplayMode> CREATOR =
+            new Parcelable.Creator<DisplayMode>() {
+        public DisplayMode createFromParcel(Parcel in) {
+            return new DisplayMode(in);
+        }
+
+        @Override
+        public DisplayMode[] newArray(int size) {
+            return new DisplayMode[size];
+        }
+    };
+
+}

core/java/aosip/os/Concierge.java
@@ -1,0 +1,145 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package aosip.os;
+
+import android.os.Parcel;
+
+/**
+ * Simply, Concierge handles your parcels and makes sure they get marshalled and unmarshalled
+ * correctly when cross IPC boundaries even when there is a version mismatch between the client
+ * sdk level and the framework implementation.
+ *
+ * <p>On incoming parcel (to be unmarshalled):
+ *
+ * <pre class="prettyprint">
+ *     ParcelInfo incomingParcelInfo = Concierge.receiveParcel(incomingParcel);
+ *     int parcelableVersion = incomingParcelInfo.getParcelVersion();
+ *
+ *     // Do unmarshalling steps here iterating over every plausible version
+ *
+ *     // Complete the process
+ *     incomingParcelInfo.complete();
+ * </pre>
+ *
+ * <p>On outgoing parcel (to be marshalled):
+ *
+ * <pre class="prettyprint">
+ *     ParcelInfo outgoingParcelInfo = Concierge.prepareParcel(incomingParcel);
+ *
+ *     // Do marshalling steps here iterating over every plausible version
+ *
+ *     // Complete the process
+ *     outgoingParcelInfo.complete();
+ * </pre>
+ */
+public final class Concierge {
+
+    /** Not instantiable */
+    private Concierge() {
+        // Don't instantiate
+    }
+
+    /**
+     * @hide
+     */
+    public static final int PARCELABLE_VERSION = "pie";
+
+    /**
+     * Tell the concierge to receive our parcel, so we can get information from it.
+     *
+     * MUST CALL {@link ParcelInfo#complete()} AFTER UNMARSHALLING.
+     *
+     * @param parcel Incoming parcel to be unmarshalled
+     * @return {@link ParcelInfo} containing parcel information, specifically the version.
+     */
+    public static ParcelInfo receiveParcel(Parcel parcel) {
+        return new ParcelInfo(parcel);
+    }
+
+    /**
+     * Prepare a parcel for the Concierge.
+     *
+     * MUST CALL {@link ParcelInfo#complete()} AFTER MARSHALLING.
+     *
+     * @param parcel Outgoing parcel to be marshalled
+     * @return {@link ParcelInfo} containing parcel information, specifically the version.
+     */
+    public static ParcelInfo prepareParcel(Parcel parcel) {
+        return new ParcelInfo(parcel, PARCELABLE_VERSION);
+    }
+
+    /**
+     * Parcel header info specific to the Parcel object that is passed in via
+     * {@link #prepareParcel(Parcel)} or {@link #receiveParcel(Parcel)}. The exposed method
+     * of {@link #getParcelVersion()} gets the api level of the parcel object.
+     */
+    public final static class ParcelInfo {
+        private Parcel mParcel;
+        private int mParcelableVersion;
+        private int mParcelableSize;
+        private int mStartPosition;
+        private int mSizePosition;
+        private boolean mCreation = false;
+
+        ParcelInfo(Parcel parcel) {
+            mCreation = false;
+            mParcel = parcel;
+            mParcelableVersion = parcel.readInt();
+            mParcelableSize = parcel.readInt();
+            mStartPosition = parcel.dataPosition();
+        }
+
+        ParcelInfo(Parcel parcel, int parcelableVersion) {
+            mCreation = true;
+            mParcel = parcel;
+            mParcelableVersion = parcelableVersion;
+
+            // Write parcelable version, make sure to define explicit changes
+            // within {@link #PARCELABLE_VERSION);
+            mParcel.writeInt(mParcelableVersion);
+
+            // Inject a placeholder that will store the parcel size from this point on
+            // (not including the size itself).
+            mSizePosition = parcel.dataPosition();
+            mParcel.writeInt(0);
+            mStartPosition = parcel.dataPosition();
+        }
+
+        /**
+         * Get the parcel version from the {@link Parcel} received by the Concierge.
+         * @return {@link #PARCELABLE_VERSION} of the {@link Parcel}
+         */
+        public int getParcelVersion() {
+            return mParcelableVersion;
+        }
+
+        /**
+         * Complete the {@link ParcelInfo} for the Concierge.
+         */
+        public void complete() {
+            if (mCreation) {
+                // Go back and write size
+                mParcelableSize = mParcel.dataPosition() - mStartPosition;
+                mParcel.setDataPosition(mSizePosition);
+                mParcel.writeInt(mParcelableSize);
+                mParcel.setDataPosition(mStartPosition + mParcelableSize);
+            } else {
+                mParcel.setDataPosition(mStartPosition + mParcelableSize);
+            }
+        }
+    }
+}