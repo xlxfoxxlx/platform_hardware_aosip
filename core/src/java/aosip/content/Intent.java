CypherOS/hardware_aoscp_extensions

devicehw: Add camera launch intent
Change-Id: I0b248791c43e8406590437bbfb496f1d086b06c1

core/src/java/aoscp/content/Intent.java
@@ -35,4 +35,13 @@ public class Intent {
      */
     public static final String ACTION_INITIALIZE_DEVICE_HARDWARE =
             "android.intent.action.INITIALIZE_DEVICE_HARDWARE";
+
+    /**
+     * Broadcast action: notify the system that the user has performed a gesture on the screen
+     * to launch the camera. Broadcast should be protected to receivers holding the
+     * {@link Manifest.permission#STATUS_BAR_SERVICE} permission.
+     * @hide
+     */
+    public static final String ACTION_SCREEN_CAMERA_GESTURE =
+            "aoscp.intent.action.SCREEN_CAMERA_GESTURE";
 }

xlxfoxxlx/platform_hardware_aosip

Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

core/src/java/aosip/content/Intent.java
@@ -35,4 +35,13 @@ public class Intent {
      */
     public static final String ACTION_INITIALIZE_DEVICE_HARDWARE =
             "android.intent.action.INITIALIZE_DEVICE_HARDWARE";
+
+    /**
+     * Broadcast action: notify the system that the user has performed a gesture on the screen
+     * to launch the camera. Broadcast should be protected to receivers holding the
+     * {@link Manifest.permission#STATUS_BAR_SERVICE} permission.
+     * @hide
+     */
+    public static final String ACTION_SCREEN_CAMERA_GESTURE =
+            "aosip.intent.action.SCREEN_CAMERA_GESTURE";
 }