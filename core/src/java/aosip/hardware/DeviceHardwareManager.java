CypherOS/hardware_aoscp_extensions

devicehw: Add support for touchscreen gestures
Touchscreen Gestures API

Change-Id: Ie047919cebac281130fe79c4c7abf17f88c7a45e

core/src/java/aoscp/hardware/DeviceHardwareManager.java
@@ -40,6 +40,7 @@ import java.util.NoSuchElementException;
 
 import vendor.aoscp.displayengine.V1_0.IDisplayModes;
 import vendor.aoscp.touch.V1_0.IFingerprintNavigation;
+import vendor.aoscp.touch.V1_0.ITouchscreenGesture;
 
 /**
  * Manages access to device hardware extensions
@@ -83,6 +84,12 @@ public final class DeviceHardwareManager {
      */
     @VisibleForTesting
     public static final int FEATURE_ALERT_SLIDER = 0x3;
+
+	/**
+     * Touchscreen gesture
+     */
+    @VisibleForTesting
+    public static final int FEATURE_TOUCHSCREEN_GESTURES = 0x4;
 
     /**
      * @hide to prevent subclassing from outside of the framework
@@ -198,6 +205,8 @@ public final class DeviceHardwareManager {
                     return IFingerprintNavigation.getService(true);
                 case FEATURE_DISPLAY_MODES:
                     return IDisplayModes.getService(true);
+				case FEATURE_TOUCHSCREEN_GESTURES:
+                    return ITouchscreenGesture.getService(true);
             }
         } catch (NoSuchElementException | RemoteException e) {
         }
@@ -400,6 +409,42 @@ public final class DeviceHardwareManager {
         } catch (RemoteException e) {
         }
         return null;
+    }
+
+	/**
+     * @return a list of available touchscreen gestures on the devices
+     */
+    public TouchscreenGesture[] getTouchscreenGestures() {
+        try {
+            if (isSupportedHIDL(FEATURE_TOUCHSCREEN_GESTURES)) {
+                ITouchscreenGesture touchscreenGesture = (ITouchscreenGesture)
+                        mHIDLMap.get(FEATURE_TOUCHSCREEN_GESTURES);
+                return HIDLHelper.fromHIDLGestures(touchscreenGesture.getSupportedGestures());
+            } else if (checkService()) {
+                return sService.getTouchscreenGestures();
+            }
+        } catch (RemoteException e) {
+        }
+        return null;
+    }
+
+    /**
+     * @return true if setting the activation status was successful
+     */
+    public boolean setTouchscreenGestureEnabled(
+            TouchscreenGesture gesture, boolean state) {
+        try {
+            if (isSupportedHIDL(FEATURE_TOUCHSCREEN_GESTURES)) {
+                ITouchscreenGesture touchscreenGesture = (ITouchscreenGesture)
+                        mHIDLMap.get(FEATURE_TOUCHSCREEN_GESTURES);
+                return touchscreenGesture.setGestureEnabled(
+                        HIDLHelper.toHIDLGesture(gesture), state);
+            } else if (checkService()) {
+                return sService.setTouchscreenGestureEnabled(gesture, state);
+            }
+        } catch (RemoteException e) {
+        }
+        return false;
     }
 
     /**

core/src/java/aoscp/hardware/HIDLHelper.java
@@ -17,6 +17,7 @@
 package aoscp.hardware;
 
 import aoscp.hardware.DisplayMode;
+import aoscp.hardware.TouchscreenGesture;
 
 import java.util.ArrayList;
 
@@ -36,5 +37,24 @@ class HIDLHelper {
     public static DisplayMode fromHIDLMode(
             vendor.aoscp.displayengine.V1_0.DisplayMode mode) {
         return new DisplayMode(mode.id, mode.name);
+    }
+
+	static TouchscreenGesture[] fromHIDLGestures(
+            ArrayList<vendor.aoscp.touch.V1_0.Gesture> gestures) {
+        int size = gestures.size();
+        TouchscreenGesture[] r = new TouchscreenGesture[size];
+        for (int i = 0; i < size; i++) {
+            vendor.aoscp.touch.V1_0.Gesture g = gestures.get(i);
+            r[i] = new TouchscreenGesture(g.id, g.name, g.keycode);
+        }
+        return r;
+    }
+
+    static vendor.aoscp.touch.V1_0.Gesture toHIDLGesture(TouchscreenGesture gesture) {
+        vendor.aoscp.touch.V1_0.Gesture g = new vendor.aoscp.touch.V1_0.Gesture();
+        g.id = gesture.id;
+        g.name = gesture.name;
+        g.keycode = gesture.keycode;
+        return g;
     }
 }

core/src/java/aoscp/hardware/IDeviceHardwareService.aidl
@@ -21,6 +21,7 @@ package aoscp.hardware;
 import android.view.KeyEvent;
 
 import aoscp.hardware.DisplayMode;
+import aoscp.hardware.TouchscreenGesture;
 
 /** @hide */
 interface IDeviceHardwareService {
@@ -38,4 +39,7 @@ interface IDeviceHardwareService {
 
     boolean triStateReady();
     KeyEvent handleTriStateEvent(in KeyEvent event);
+
+	TouchscreenGesture[] getTouchscreenGestures();
+    boolean setTouchscreenGestureEnabled(in TouchscreenGesture gesture, boolean state);
 }

core/src/java/aoscp/hardware/TouchscreenGesture.aidl
@@ -1,0 +1,21 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ *               2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package aoscp.hardware;
+
+parcelable TouchscreenGesture;

core/src/java/aoscp/hardware/TouchscreenGesture.java
@@ -1,0 +1,78 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ *               2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package aoscp.hardware;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * Touchscreen gestures API
+ *
+ * A device may implement several touchscreen gestures for use while
+ * the display is turned off, such as drawing alphabets and shapes.
+ * These gestures can be interpreted by userspace to activate certain
+ * actions and launch certain apps, such as to skip music tracks,
+ * to turn on the flashlight, or to launch the camera app.
+ *
+ * This *should always* be supported by the hardware directly.
+ * A lot of recent touch controllers have a firmware option for this.
+ *
+ * This API provides support for enumerating the gestures
+ * supported by the touchscreen.
+ *
+ * A TouchscreenGesture is referenced by it's identifier and carries an
+ * associated name (up to the user to translate this value).
+ */
+public class TouchscreenGesture implements Parcelable {
+
+    public final int id;
+    public final String name;
+    public final int keycode;
+
+    public TouchscreenGesture(int id, String name, int keycode) {
+        this.id = id;
+        this.name = name;
+        this.keycode = keycode;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int flags) {
+        parcel.writeInt(id);
+        parcel.writeString(name);
+        parcel.writeInt(keycode);
+    }
+
+    /** @hide */
+    public static final Parcelable.Creator<TouchscreenGesture> CREATOR =
+            new Parcelable.Creator<TouchscreenGesture>() {
+
+        public TouchscreenGesture createFromParcel(Parcel in) {
+            return new TouchscreenGesture(in.readInt(), in.readString(), in.readInt());
+        }
+
+        @Override
+        public TouchscreenGesture[] newArray(int size) {
+            return new TouchscreenGesture[size];
+        }
+    };
+}

services/src/java/co/aoscp/server/DeviceHardwareService.java
@@ -32,10 +32,12 @@ import aoscp.content.HardwareIntent;
 import aoscp.hardware.DeviceHardwareManager;
 import aoscp.hardware.DisplayMode;
 import aoscp.hardware.IDeviceHardwareService;
+import aoscp.hardware.TouchscreenGesture;
 
 import co.aoscp.hwcontrollers.AlertSliderController;
 import co.aoscp.hwcontrollers.DisplayEngineController;
 import co.aoscp.hwcontrollers.FingerprintNavigationController;
+import co.aoscp.hwcontrollers.TouchscreenGestureController;
 
 import com.android.server.HwSystemService;
 
@@ -66,6 +68,10 @@ public class DeviceHardwareService extends HwSystemService {
         // Alert Slider
         public boolean triStateReady(Context context);
         public KeyEvent handleTriStateEvent(KeyEvent event);
+
+		// Touchscreen Gestures
+		public TouchscreenGesture[] getTouchscreenGestures();
+        public boolean setTouchscreenGestureEnabled(TouchscreenGesture gesture, boolean state);
     }
 
     private class LegacyHardware implements HardwareInterface {
@@ -79,6 +85,8 @@ public class DeviceHardwareService extends HwSystemService {
                 mSupportedFeatures |= DeviceHardwareManager.FEATURE_FINGERPRINT_NAVIGATION;
             if (AlertSliderController.isSupported())
                 mSupportedFeatures |= DeviceHardwareManager.FEATURE_ALERT_SLIDER;
+			if (TouchscreenGestureController.isSupported())
+                mSupportedFeatures |= DeviceHardwareManager.FEATURE_TOUCHSCREEN_GESTURES;
         }
 
         public int getSupportedFeatures() {
@@ -125,6 +133,14 @@ public class DeviceHardwareService extends HwSystemService {
 
         public KeyEvent handleTriStateEvent(KeyEvent event) {
             return AlertSliderController.handleTriStateEvent(event);
+        }
+
+		public TouchscreenGesture[] getTouchscreenGestures() {
+            return TouchscreenGestureController.getAvailableGestures();
+        }
+
+        public boolean setTouchscreenGestureEnabled(TouchscreenGesture gesture, boolean state) {
+            return TouchscreenGestureController.setGestureEnabled(gesture, state);
         }
     }
 
@@ -268,6 +284,28 @@ public class DeviceHardwareService extends HwSystemService {
                 return null;
             }
             return mHwImpl.handleTriStateEvent(event);
+        }
+
+		@Override
+        public TouchscreenGesture[] getTouchscreenGestures() {
+            mContext.enforceCallingOrSelfPermission(
+                    android.Manifest.permission.DEVICE_HARDWARE_ACCESS, null);
+            if (!isSupported(DeviceHardwareManager.FEATURE_TOUCHSCREEN_GESTURES)) {
+                Log.e(TAG, "Touchscreen gestures are not supported");
+                return null;
+            }
+            return mHwImpl.getTouchscreenGestures();
+        }
+
+        @Override
+        public boolean setTouchscreenGestureEnabled(TouchscreenGesture gesture, boolean state) {
+            mContext.enforceCallingOrSelfPermission(
+                    android.Manifest.permission.DEVICE_HARDWARE_ACCESS, null);
+            if (!isSupported(DeviceHardwareManager.FEATURE_TOUCHSCREEN_GESTURES)) {
+                Log.e(TAG, "Touchscreen gestures are not supported");
+                return false;
+            }
+            return mHwImpl.setTouchscreenGestureEnabled(gesture, state);
         }
     };
 }

src/co/aoscp/hwcontrollers/TouchscreenGestureController.java
@@ -1,0 +1,74 @@
+/*
+ * Copyright (C) 2018 CypherOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package co.aoscp.hwcontrollers;
+
+import aoscp.hardware.TouchscreenGesture;
+
+/**
+ * Touchscreen gestures API
+ *
+ * A device may implement several touchscreen gestures for use while
+ * the display is turned off, such as drawing alphabets and shapes.
+ * These gestures can be interpreted by userspace to activate certain
+ * actions and launch certain apps, such as to skip music tracks,
+ * to turn on the flashlight, or to launch the camera app.
+ *
+ * This *should always* be supported by the hardware directly.
+ * A lot of recent touch controllers have a firmware option for this.
+ *
+ * This API provides support for enumerating the gestures
+ * supported by the touchscreen.
+ */
+public class TouchscreenGestureController {
+
+    /**
+     * Whether device supports touchscreen gestures
+     *
+     * @return boolean Supported devices must return always true
+     */
+    public static boolean isSupported() {
+        return false;
+    }
+
+    /**
+     * Get the list of available gestures. A mode has an integer
+     * identifier and a string name.
+     *
+     * It is the responsibility of the upper layers to
+     * map the name to a human-readable format or perform translation.
+     *
+     * @return TouchscreenGesture[] An array of the touchscreen gestures
+     *                              available on a device
+     */
+    public static TouchscreenGesture[] getAvailableGestures() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * This method allows to set the activation status of a gesture
+     *
+     * @param gesture The gesture to be activated
+     *        state   The new activation status of the gesture
+     *
+     * @return boolean Must be false if gesture is not supported
+     *         or the operation failed; true in any other case.
+     */
+    public static boolean setGestureEnabled(
+            final TouchscreenGesture gesture, final boolean state) {
+        return false;
+    }
+}

xlxfoxxlx/platform_hardware_aosip

Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

core/src/java/aosip/hardware/DeviceHardwareManager.java
@@ -40,6 +40,7 @@ import java.util.NoSuchElementException;
 
 import vendor.aosip.displayengine.V1_0.IDisplayModes;
 import vendor.aosip.touch.V1_0.IFingerprintNavigation;
+import vendor.aosip.touch.V1_0.ITouchscreenGesture;
 
 /**
  * Manages access to device hardware extensions
@@ -80,6 +81,12 @@ public final class DeviceHardwareManager {
      */
     @VisibleForTesting
     public static final int FEATURE_ALERT_SLIDER = 0x3;
+
+    /**
+     * Touchscreen gesture
+     */
+    @VisibleForTesting
+    public static final int FEATURE_TOUCHSCREEN_GESTURES = 0x4;
 
     private static final List<Integer> BOOLEAN_FEATURES = Arrays.asList(
         FEATURE_FINGERPRINT_NAVIGATION
@@ -199,6 +206,8 @@ public final class DeviceHardwareManager {
                     return IFingerprintNavigation.getService(true);
                 case FEATURE_DISPLAY_MODES:
                     return IDisplayModes.getService(true);
+                case FEATURE_TOUCHSCREEN_GESTURES:
+                    return ITouchscreenGesture.getService(true);
             }
         } catch (NoSuchElementException | RemoteException e) {
         }
@@ -401,6 +410,42 @@ public final class DeviceHardwareManager {
         } catch (RemoteException e) {
         }
         return null;
+    }
+
+    /**
+     * @return a list of available touchscreen gestures on the devices
+     */
+    public TouchscreenGesture[] getTouchscreenGestures() {
+        try {
+            if (isSupportedHIDL(FEATURE_TOUCHSCREEN_GESTURES)) {
+                ITouchscreenGesture touchscreenGesture = (ITouchscreenGesture)
+                        mHIDLMap.get(FEATURE_TOUCHSCREEN_GESTURES);
+                return HIDLHelper.fromHIDLGestures(touchscreenGesture.getSupportedGestures());
+            } else if (checkService()) {
+                return sService.getTouchscreenGestures();
+            }
+        } catch (RemoteException e) {
+        }
+        return null;
+    }
+
+    /**
+     * @return true if setting the activation status was successful
+     */
+    public boolean setTouchscreenGestureEnabled(
+            TouchscreenGesture gesture, boolean state) {
+        try {
+            if (isSupportedHIDL(FEATURE_TOUCHSCREEN_GESTURES)) {
+                ITouchscreenGesture touchscreenGesture = (ITouchscreenGesture)
+                        mHIDLMap.get(FEATURE_TOUCHSCREEN_GESTURES);
+                return touchscreenGesture.setGestureEnabled(
+                        HIDLHelper.toHIDLGesture(gesture), state);
+            } else if (checkService()) {
+                return sService.setTouchscreenGestureEnabled(gesture, state);
+            }
+        } catch (RemoteException e) {
+        }
+        return false;
     }
 
     /**

core/src/java/aosip/hardware/HIDLHelper.java
@@ -17,6 +17,7 @@
 package aosip.hardware;
 
 import aosip.hardware.DisplayMode;
+import aosip.hardware.TouchscreenGesture;
 
 import java.util.ArrayList;
 
@@ -36,5 +37,24 @@ class HIDLHelper {
     public static DisplayMode fromHIDLMode(
             vendor.aosip.displayengine.V1_0.DisplayMode mode) {
         return new DisplayMode(mode.id, mode.name);
+    }
+
+    static TouchscreenGesture[] fromHIDLGestures(
+            ArrayList<vendor.aosip.touch.V1_0.Gesture> gestures) {
+        int size = gestures.size();
+        TouchscreenGesture[] r = new TouchscreenGesture[size];
+        for (int i = 0; i < size; i++) {
+            vendor.aosip.touch.V1_0.Gesture g = gestures.get(i);
+            r[i] = new TouchscreenGesture(g.id, g.name, g.keycode);
+        }
+        return r;
+    }
+
+    static vendor.aosip.touch.V1_0.Gesture toHIDLGesture(TouchscreenGesture gesture) {
+        vendor.aosip.touch.V1_0.Gesture g = new vendor.aosip.touch.V1_0.Gesture();
+        g.id = gesture.id;
+        g.name = gesture.name;
+        g.keycode = gesture.keycode;
+        return g;
     }
 }

core/src/java/aosip/hardware/IDeviceHardwareService.aidl
@@ -20,7 +20,8 @@ package aosip.hardware;
 
 import android.view.KeyEvent;
 
-import aoscp.hardware.DisplayMode;
+import aosip.hardware.DisplayMode;
+import aosip.hardware.TouchscreenGesture;
 
 /** @hide */
 interface IDeviceHardwareService {
@@ -38,4 +39,7 @@ interface IDeviceHardwareService {
 
     boolean triStateReady();
     KeyEvent handleTriStateEvent(in KeyEvent event);
+
+    TouchscreenGesture[] getTouchscreenGestures();
+    boolean setTouchscreenGestureEnabled(in TouchscreenGesture gesture, boolean state);
 }

core/src/java/aosip/hardware/TouchscreenGesture.aidl
@@ -1,0 +1,21 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ *               2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package aosip.hardware;
+
+parcelable TouchscreenGesture;

core/src/java/aosip/hardware/TouchscreenGesture.java
@@ -1,0 +1,78 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ *               2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package aosip.hardware;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * Touchscreen gestures API
+ *
+ * A device may implement several touchscreen gestures for use while
+ * the display is turned off, such as drawing alphabets and shapes.
+ * These gestures can be interpreted by userspace to activate certain
+ * actions and launch certain apps, such as to skip music tracks,
+ * to turn on the flashlight, or to launch the camera app.
+ *
+ * This *should always* be supported by the hardware directly.
+ * A lot of recent touch controllers have a firmware option for this.
+ *
+ * This API provides support for enumerating the gestures
+ * supported by the touchscreen.
+ *
+ * A TouchscreenGesture is referenced by it's identifier and carries an
+ * associated name (up to the user to translate this value).
+ */
+public class TouchscreenGesture implements Parcelable {
+
+    public final int id;
+    public final String name;
+    public final int keycode;
+
+    public TouchscreenGesture(int id, String name, int keycode) {
+        this.id = id;
+        this.name = name;
+        this.keycode = keycode;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int flags) {
+        parcel.writeInt(id);
+        parcel.writeString(name);
+        parcel.writeInt(keycode);
+    }
+
+    /** @hide */
+    public static final Parcelable.Creator<TouchscreenGesture> CREATOR =
+            new Parcelable.Creator<TouchscreenGesture>() {
+
+        public TouchscreenGesture createFromParcel(Parcel in) {
+            return new TouchscreenGesture(in.readInt(), in.readString(), in.readInt());
+        }
+
+        @Override
+        public TouchscreenGesture[] newArray(int size) {
+            return new TouchscreenGesture[size];
+        }
+    };
+}

services/src/java/co/aosip/server/DeviceHardwareService.java
@@ -32,10 +32,12 @@ import aosip.content.HardwareIntent;
 import aosip.hardware.DeviceHardwareManager;
 import aosip.hardware.DisplayMode;
 import aosip.hardware.IDeviceHardwareService;
+import aosip.hardware.TouchscreenGesture;
 
 import co.aosip.hwcontrollers.AlertSliderController;
 import co.aosip.hwcontrollers.DisplayEngineController;
 import co.aosip.hwcontrollers.FingerprintNavigationController;
+import co.aosip.hwcontrollers.TouchscreenGestureController;
 
 import com.android.server.HwSystemService;
 
@@ -66,6 +68,10 @@ public class DeviceHardwareService extends HwSystemService {
         // Alert Slider
         public boolean triStateReady(Context context);
         public KeyEvent handleTriStateEvent(KeyEvent event);
+
+        // Touchscreen Gestures
+        public TouchscreenGesture[] getTouchscreenGestures();
+        public boolean setTouchscreenGestureEnabled(TouchscreenGesture gesture, boolean state);
     }
 
     private class LegacyHardware implements HardwareInterface {
@@ -79,6 +85,8 @@ public class DeviceHardwareService extends HwSystemService {
                 mSupportedFeatures |= DeviceHardwareManager.FEATURE_FINGERPRINT_NAVIGATION;
             if (AlertSliderController.isSupported())
                 mSupportedFeatures |= DeviceHardwareManager.FEATURE_ALERT_SLIDER;
+            if (TouchscreenGestureController.isSupported())
+                mSupportedFeatures |= DeviceHardwareManager.FEATURE_TOUCHSCREEN_GESTURES;
         }
 
         public int getSupportedFeatures() {
@@ -125,6 +133,14 @@ public class DeviceHardwareService extends HwSystemService {
 
         public KeyEvent handleTriStateEvent(KeyEvent event) {
             return AlertSliderController.handleTriStateEvent(event);
+        }
+
+        public TouchscreenGesture[] getTouchscreenGestures() {
+            return TouchscreenGestureController.getAvailableGestures();
+        }
+
+        public boolean setTouchscreenGestureEnabled(TouchscreenGesture gesture, boolean state) {
+            return TouchscreenGestureController.setGestureEnabled(gesture, state);
         }
     }
 
@@ -268,6 +284,28 @@ public class DeviceHardwareService extends HwSystemService {
                 return null;
             }
             return mHwImpl.handleTriStateEvent(event);
+        }
+
+        @Override
+        public TouchscreenGesture[] getTouchscreenGestures() {
+            mContext.enforceCallingOrSelfPermission(
+                    android.Manifest.permission.DEVICE_HARDWARE_ACCESS, null);
+            if (!isSupported(DeviceHardwareManager.FEATURE_TOUCHSCREEN_GESTURES)) {
+                Log.e(TAG, "Touchscreen gestures are not supported");
+                return null;
+            }
+            return mHwImpl.getTouchscreenGestures();
+        }
+
+        @Override
+        public boolean setTouchscreenGestureEnabled(TouchscreenGesture gesture, boolean state) {
+            mContext.enforceCallingOrSelfPermission(
+                    android.Manifest.permission.DEVICE_HARDWARE_ACCESS, null);
+            if (!isSupported(DeviceHardwareManager.FEATURE_TOUCHSCREEN_GESTURES)) {
+                Log.e(TAG, "Touchscreen gestures are not supported");
+                return false;
+            }
+            return mHwImpl.setTouchscreenGestureEnabled(gesture, state);
         }
     };
 }

src/co/aosip/hwcontrollers/TouchscreenGestureController.java
@@ -1,0 +1,74 @@
+/*
+ * Copyright (C) 2018 CypherOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package co.aosip.hwcontrollers;
+
+import aosip.hardware.TouchscreenGesture;
+
+/**
+ * Touchscreen gestures API
+ *
+ * A device may implement several touchscreen gestures for use while
+ * the display is turned off, such as drawing alphabets and shapes.
+ * These gestures can be interpreted by userspace to activate certain
+ * actions and launch certain apps, such as to skip music tracks,
+ * to turn on the flashlight, or to launch the camera app.
+ *
+ * This *should always* be supported by the hardware directly.
+ * A lot of recent touch controllers have a firmware option for this.
+ *
+ * This API provides support for enumerating the gestures
+ * supported by the touchscreen.
+ */
+public class TouchscreenGestureController {
+
+    /**
+     * Whether device supports touchscreen gestures
+     *
+     * @return boolean Supported devices must return always true
+     */
+    public static boolean isSupported() {
+        return false;
+    }
+
+    /**
+     * Get the list of available gestures. A mode has an integer
+     * identifier and a string name.
+     *
+     * It is the responsibility of the upper layers to
+     * map the name to a human-readable format or perform translation.
+     *
+     * @return TouchscreenGesture[] An array of the touchscreen gestures
+     *                              available on a device
+     */
+    public static TouchscreenGesture[] getAvailableGestures() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * This method allows to set the activation status of a gesture
+     *
+     * @param gesture The gesture to be activated
+     *        state   The new activation status of the gesture
+     *
+     * @return boolean Must be false if gesture is not supported
+     *         or the operation failed; true in any other case.
+     */
+    public static boolean setGestureEnabled(
+            final TouchscreenGesture gesture, final boolean state) {
+        return false;
+    }
+}